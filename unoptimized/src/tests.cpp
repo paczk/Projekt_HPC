2c1b305d25e4d240a75d577f7e66011df1c4b490
paczk <paczka1234567890@gmail.com>
2025-06-19 10:56:45 +0200

    changeg testing

--- unoptimized/src/tests.cpp
+++ unoptimized/src/tests.cpp
@@ -9,22 +9,39 @@ int main()
     MultibodySystem sys;
 
     // Define some bodies and joints
-    Body body1(1, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0);
-    Body body2(2, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0);
+    //Body body1(1, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0);
+    //Body body2(2, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0);
+
     
+
+    for(int i = 0; i < 100000; ++i)
+    {
+        Body body1(i + 1, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0);
+        
+        sys.addBody(body1);
+    }
     
-    FixedPositionConstraint constraint1(1, 1, Eigen::Vector3d(0.0, 0.0, 0.0));
-    FixedPositionConstraint constraint2(2, 2, Eigen::Vector3d(1.0, 0.0, 0.0));
-    FixedOrientationConstraint constraint3(3, 1, Eigen::Vector3d(0.0, 0.0, 1.0));
-    FixedOrientationConstraint constraint4(4, 2, Eigen::Vector3d(0.0, 1.0, 0.0));
+    //FixedPositionConstraint constraint1(1, 1, Eigen::Vector3d(0.0, 0.0, 0.0));
+    //FixedPositionConstraint constraint2(2, 2, Eigen::Vector3d(1.0, 0.0, 0.0));
+    //FixedOrientationConstraint constraint3(3, 1, Eigen::Vector3d(0.0, 0.0, 1.0));
+    //FixedOrientationConstraint constraint4(4, 2, Eigen::Vector3d(0.0, 1.0, 0.0));
+
+    for(int i = 0; i < 100000; ++i)
+    {
+        FixedPositionConstraint constraint1(i + 1, i + 1, Eigen::Vector3d(0.0, 0.0, 0.0));
+        FixedOrientationConstraint constraint3(i + 100000, i + 1, Eigen::Vector3d(0.0, 0.0, 1.0));
+
+        sys.addConstraint(constraint1);
+        sys.addConstraint(constraint3);
+    }
 
     // Add bodies and joints to the solver
-    sys.addBody(body1);
-    sys.addBody(body2);
-    sys.addConstraint(constraint1);
-    sys.addConstraint(constraint2);
-    sys.addConstraint(constraint3);
-    sys.addConstraint(constraint4);
+    //sys.addBody(body1);
+    //sys.addBody(body2);
+    //sys.addConstraint(constraint1);
+    //sys.addConstraint(constraint2);
+    //sys.addConstraint(constraint3);
+    //sys.addConstraint(constraint4);
 
     // Solve the multibody system
     auto output = multibody_solver(sys, 0.0);
